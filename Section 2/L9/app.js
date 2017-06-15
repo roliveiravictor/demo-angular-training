d9d2f11c16ea07608afb39e71e88ebc8886cd97c
Victor Oliveira <victordext@gmail.com>
2017-06-15 11:05:02 -0300

    Final Version - Section 2

--- Section 2/L9/app.js
+++ Section 2/L9/app.js
@@ -1,3 +1,15 @@
+/**********************************************************************/
+/*                                                                    */
+/*  			Angular Training                              */
+/*                                                                    */
+/*    My intention with these codes was to create a web app with      */
+/*    Angular 1.0. The codes here presented were written watching     */
+/*    Anthony Alicea's course at Udemy. For more information please   */
+/*    visit https://www.udemy.com/learn-angularjs                     */
+/*                                                                    */
+/*                                                                    */
+/**********************************************************************/
+
 // MODULE
 var angularTrial = angular.module('angularTrial', []);
 
